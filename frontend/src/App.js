faf0ced8456a0d147522df49d97f1ec2cbae9703
awolfen <alfie.wolfenden@gmail.com>
2021-08-25 19:56:00 +0100

    'routing'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import NewPlace from './places/pages/NewPlace';
+import Users from './user/pages/Users';
+
+const App = () => {
   return (
-    <div className="App">
-      <h2>Lets go</h2>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
