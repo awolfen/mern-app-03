d13287beee1abffe60bd7f1b74b49535dba28521
awolfen <alfie.wolfenden@gmail.com>
2021-08-27 00:01:28 +0100

    'stylings'

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Minsc',
+            image: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/09/Minsc.jpg/200px-Minsc.jpg',
+            places: '2'
+        }
+    ];
     return (
-        <h2>Users</h2>
+        <React.Fragment>
+            <h2>Users</h2>
+            <UsersList items={USERS} />
+        </React.Fragment>
     );
 };
 
